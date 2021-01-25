22d26aef4ed1efc7d528005010c119e8c8dc7312
Hiplice <30780500+Hiplice@users.noreply.github.com>
2021-01-25 19:38:41 +0300

    Revert "Revert "made some changes""
    This reverts commit 036b1992446c088216d99e27a99e51a9e666d553.

--- tests/urls.py
+++ tests/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.show_tests, name='show tests'),
     re_path(r'(?P<test_number>\d+)/', views.start_test, name='start test'),
+    path('add', views.add_test, name='add test')
 ]
