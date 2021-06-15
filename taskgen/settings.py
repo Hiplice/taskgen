397cd5e73ae1970fbfee3c47fda7aa63d84825a6
Hiplice <30780500+Hiplice@users.noreply.github.com>
2021-06-15 20:14:48 +0300

    upd

--- taskgen/settings.py
+++ taskgen/settings.py
@@ -125,10 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
